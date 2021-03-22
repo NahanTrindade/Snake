42846983a4c23aef1f45b1a5e095febb2818fbe2
NahanTrindade <nahantrindade.p@gmail.com>
2021-03-21 20:11:50 -0400

    Update script.js

--- script.js
+++ script.js
@@ -57,6 +57,13 @@ function iniciarJogo(){
     if(snake[0].y > 15 * box && direction == "down") snake[0].y = 0;
     if(snake[0].y < 0 && direction == "up") snake[0].y = 16 * box;
 
+    for(i = 1; i < snake.length; i++){
+        if(snake[0].x == snake[i].x && snake[0].y == snake[i].y){
+            clearInterval(jogo);
+            alert('Game Over');
+        }
+    }
+
     criarBG();
     criarCobrinha();
     drawFood();
@@ -69,7 +76,13 @@ function iniciarJogo(){
     if(direction == "up") snakey -= box;
     if(direction == "down") snakey += box;
 
-    snake.pop();
+    if(snakex != food.x || snakey != food.y){
+        snake.pop();
+    }else{food.x = Math.floor(Math.random() * 15+1)*box;
+       food.y = Math.floor(Math.random()*15+1)*box; 
+    }
+    
+    
     
     let newhead = {
         x: snakex,
